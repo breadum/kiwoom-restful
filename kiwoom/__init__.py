9ba71c846990ab7c869c05a170a4ec9f93a602b8
breadum <breadum.kr@gmail.com>
2025-09-13 03:01:00 +0900

    control entire namespace for the module

--- kiwoom/__init__.py
+++ kiwoom/__init__.py
@@ -1,5 +1,9 @@
-from .__version__ import __version__
+from kiwoom import config as config
+from kiwoom.api import API as API
+from kiwoom.bot import Bot as Bot
+from kiwoom.config import MOCK as MOCK
+from kiwoom.config import REAL as REAL
 
-from kiwoom.api import API
-from kiwoom.bot import Bot
-from kiwoom.config import REAL
+from .__version__ import __version__ as __version__
+
+__all__ = ["API", "Bot", "MOCK", "REAL", "__version__", "config"]
